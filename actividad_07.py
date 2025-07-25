5c313684ea569c88aeb2733b04678a9c71f0be9f
Aileen127 <fleuraileen394@gmail.com>
2025-07-24 21:44:28 -0600

    Inicio de codigo!

---
+++ actividad_07.py
@@ -0,0 +1,9 @@
+
+def suma_total(numero): #1.1
+    suma = 0
+    for n in numero:
+        if numero != 0:
+            suma += n
+    return suma
+def promedio(numero):# 1.2
+    return sum(numero) / len(numero)
